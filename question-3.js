28a82cdb01112b5825811ffe7aa38d1038c323c9
James <jamespond707@gmail.comm>
2026-02-17 10:19:55 +0700

    feat: implement solutions for algorithm questions 1, 2, and 4, and add commit history log.

--- question-3.js
+++ question-3.js
@@ -1,20 +1,20 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
-
+เป็น O(n) เพราะว่ามันลูปผ่านข้อมูลทั้งหมดครั้งเดียว (linear) 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
-
+เป็น O(log n) เพราะมันลูปผ่านข้อมูลทีละครึ่ง (binary search)
 
 */
 
 /* 
 Which function is more efficient and why?
 Answer:
-
+findProductPrice มีประสิทธิภาพมากกว่า เพราะมีการตัดจำนวนการลูปทุกครั้งที่ยังไม่เจอข้อมูลที่ต้องการ
 
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
-
+เป็น O(n^2) เพราะว่ามันลูปผ่านข้อมูลทั้งหมด 2 ครั้ง เป็นการลูปแบบ nested loop
 
 */
